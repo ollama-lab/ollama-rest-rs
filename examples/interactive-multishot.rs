e48f14268028bb2cbe747a618c1659f6a81351b2
Charles Dong <chardon_cs@proton.me>
2024-07-14 19:36:20 +0800

    Add interactive chatbot example

--- examples/interactive-multishot.rs
+++ examples/interactive-multishot.rs
@@ -1,13 +1,73 @@
 //! Your LLM might mention the "previous talk" in this chat!
 
-use ollama_rest::{models::chat::ChatRequest, Ollama};
+use std::io::{BufRead, Write};
+
+use ollama_rest::{prelude::*, Ollama};
 use serde_json::json;
+
+const MODEL_NAME: &str = "llama3";
 
 #[tokio::main]
 async fn main() {
     // Make sure Ollama serves at 127.0.0.1:11434
     let ollama = Ollama::default();
 
-    let request = serde_json::from_value::<ChatRequest>(json!({
-    }));
+    let mut messages = Vec::<Message>::new();
+    let stdin = std::io::stdin();
+    let mut stdout = std::io::stdout();
+
+    print!("Loading model... ");
+    stdout.flush().unwrap();
+
+    ollama.load_model(MODEL_NAME).await.unwrap();
+
+    println!("done");
+
+    loop {
+        let mut prompt = String::new();
+
+        print!("\n>>> ");
+        stdout.flush().unwrap();
+
+        // User prompt input
+        stdin.lock().read_line(&mut prompt).unwrap();
+
+        // Exit when user typed "/bye"
+        if prompt.starts_with("/bye") {
+            break;
+        }
+
+        messages.push(Message {
+            role: Role::User,
+            content: prompt,
+            images: None,
+        });
+
+        let mut completion = String::new();
+
+        println!();
+
+        // Send conversation to the LLM
+        ollama.chat(&serde_json::from_value::<ChatRequest>(json!({
+            "model": MODEL_NAME,
+            "messages": messages,
+        })).unwrap(), Some(|res: &ChatResponse| {
+            if !res.done {
+                if let Some(msg) = &res.message {
+                    print!("{}", msg.content);
+                    stdout.flush().unwrap();
+                    
+                    completion.push_str(msg.content.as_str());
+                }
+            }
+        })).await.unwrap();
+
+        println!();
+
+        messages.push(Message {
+            role: Role::Assistant,
+            content: completion,
+            images: None,
+        });
+    }
 }
