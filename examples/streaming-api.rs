d26e9df8b8a8f308b5b535f81bbcade372358ce8
Charles Dong <chardon_cs@proton.me>
2024-07-19 15:54:23 +0800

    Add streaming API example

---
+++ examples/streaming-api.rs
@@ -0,0 +1,52 @@
+//! This example shows how to build a simple REST API for relaying requests and streamed responses.
+//!
+//! [Axum](https://github.com/tokio-rs/axum) is used for Server-Sent Events (SSE).
+
+use axum::{response::{sse::Event, Sse}, routing::post, Json, Router};
+use futures::{Stream, TryStreamExt};
+use lazy_static::lazy_static;
+use ollama_rest::{errors::Error, models::chat::ChatRequest, Ollama};
+use tokio::net::TcpListener;
+
+const HOST_ADDR: &str = "127.0.0.1:9890";
+
+lazy_static! {
+    static ref API: Ollama = Ollama::default();
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/", post(chat));
+
+    println!("Server listening at {HOST_ADDR}. Press Ctrl+C to exit.");
+    println!();
+    println!(r#"===
+HOW TO USE THIS EXAMPLE:
+===
+1. Make sure the Ollama server is on!
+2. Try calling this API using cURL, or Postman... (whatever you like :D)
+For example:
+```
+curl -X POST http://127.0.0.1:9890/ -H 'Content-Type: application/json' -d '{{
+  "model": "llama3",
+  "messages": [
+    {{
+      "role": "user",
+      "content": "why is the sky blue?"
+    }}
+  ]
+}}'
+```"#);
+
+    let listener = TcpListener::bind(HOST_ADDR).await.unwrap();
+
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn chat(Json(payload): Json<ChatRequest>) -> Sse<impl Stream<Item = Result<Event, Error>>> {
+    Sse::new(
+        API.chat_streamed(&payload).await.unwrap()
+            .map_ok(|res| Event::default().json_data(res).unwrap()),
+    )
+}
